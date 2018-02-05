0c6dda157178cc8a36c12ea854454e92d8698fc8
Aashirya1995 <ak1107@wildcats.unh.edu>
2018-02-05 14:58:30 -0500

    adding views.py file

--- django/week2/portfolio/views.py
+++ django/week2/portfolio/views.py
@@ -7,3 +7,27 @@ def home(request):
     welcome = "Welcome to Aashirya's Portfolio"
     context = {'our_greeting':welcome}
     return render(request, 'home.html', context)
+
+def resume(request):
+    '''
+    Renders the Resume
+    '''
+    resume_one = "Resume"
+    context = {'my_resume':resume_one}
+    return render(request, 'resume.html', context)
+
+def portfolio(request):
+    '''
+    Renders the Portfolio
+    '''
+    portfolio_one = "Portfolio"
+    context = {'my_portfolio' : portfolio_one}
+    return render(request, 'portfolio.html', context)
+
+def contact(request):
+    '''
+    Renders the contact form template
+    '''
+    contact_one = "Contact"
+    context = {'my_contact': contact_one}
+    return render(request, 'contact.html', context)
