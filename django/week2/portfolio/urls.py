297104fa567ee24d532127065a2d992554ca72b1
Aashirya1995 <ak1107@wildcats.unh.edu>
2018-02-05 14:56:32 -0500

    adding the urls.py file

--- django/week2/portfolio/urls.py
+++ django/week2/portfolio/urls.py
@@ -18,6 +18,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('home.html', views.home, name='home'),
+    path('resume.html', views.resume, name='resume'),
+    path('portfolio.html', views.portfolio, name='portfolio'),
+    path('contact.html', views.contact, name='contact'),
     path('admin/', admin.site.urls),
 ]
