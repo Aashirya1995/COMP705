8b9509d77172ad81d4901dfd4c2d35204c2cdea7
Aashirya1995 <ak1107@wildcats.unh.edu>
2018-02-12 15:52:56 -0500

    Fixing the error of having two tabs for Experience and Education

--- django/week3/resume/urls.py
+++ django/week3/resume/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from . import views
 app_name = 'resume'
 urlpatterns = [
-    path('experience/', views.experience, name='experience'),
-    path('education/', views.education, name='education')
+    path('portfolio/', views.portfolio, name='portfolio'),
+    #path('education/', views.education, name='education')
     #path('resume.html', views.resume, name='resume'),
     #path('portfolio.html', views.portfolio, name='portfolio'),
     #path('contact.html', views.contact, name='contact'),
--- django/week3/resume/views.py
+++ django/week3/resume/views.py
@@ -2,15 +2,10 @@ from django.shortcuts import render
 from .models import Experience, Education
 
 # Create your views here.
-def experience(request):
+def portfolio(request):
 #def resume(request):
 #    return HttpResponse("Welcome to Resume App")
     qs = Experience.objects.all()
-    context = {'Experience' : qs}
-    return render(request,'resume/experience.html',context)
-def education(request):
-#def resume(request):
-#    return HttpResponse("Welcome to Resume App")
-    qs = Education.objects.all()
-    context = {'Education' : qs}
-    return render(request,'resume/education.html',context)
+    eduqs = Education.objects.all()
+    context = {'Experience' : qs, 'Education' : eduqs}
+    return render(request,'resume/portfolio.html',context)
