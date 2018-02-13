4c97b9dbf32f8df11b2fcd1542803fa05a8a9f98
Aashirya1995 <ak1107@wildcats.unh.edu>
2018-02-13 11:31:51 -0500

    Fixing the urls

--- django/week3/resume/urls.py
+++ django/week3/resume/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 app_name = 'resume'
 urlpatterns = [
-    path('portfolio/', views.portfolio, name='portfolio'),
+    path('resume/', views.resume, name='resume'),
     #path('education/', views.education, name='education')
     #path('resume.html', views.resume, name='resume'),
     #path('portfolio.html', views.portfolio, name='portfolio'),
