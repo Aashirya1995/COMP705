b2d1ab78352d9bbf154a655bf41bdb685c56ddf1
Aashirya1995 <ak1107@wildcats.unh.edu>
2018-02-13 11:29:59 -0500

    changing the name of the views function to resume

--- django/week3/resume/views.py
+++ django/week3/resume/views.py
@@ -2,10 +2,10 @@ from django.shortcuts import render
 from .models import Experience, Education
 
 # Create your views here.
-def portfolio(request):
+def resume(request):
 #def resume(request):
 #    return HttpResponse("Welcome to Resume App")
     qs = Experience.objects.all()
     eduqs = Education.objects.all()
     context = {'Experience' : qs, 'Education' : eduqs}
-    return render(request,'resume/portfolio.html',context)
+    return render(request,'resume/resume.html',context)
