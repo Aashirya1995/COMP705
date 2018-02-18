aecde18ad3df4287651f7f3df39024d5f91c76ae
Aashirya1995 <ak1107@wildcats.unh.edu>
2018-02-18 13:33:44 -0500

    Adding test.py

--- django/week4/resume/tests.py
+++ django/week4/resume/tests.py
@@ -4,6 +4,7 @@ from .models import Resume, Experience, Education
 # Create your tests here.
 class ResumeTestCase(TestCase):
     """
+    Test cases for the Models and the new methods
     """
     def setUp(self):
 
@@ -15,20 +16,32 @@ class ResumeTestCase(TestCase):
         #my_experience.save()
 
     def test_last_name_first_name(self):
+        '''
+        Test case for get_last_name_first_name
+        '''
         r = Resume.objects.first()
         self.assertEqual(r.get_last_name_first_name(), 'Kaushik Aashirya')
 
     def test_full_name(self):
+        '''
+        Test case for get_full_name method
+        '''
         r = Resume.objects.first()
         self.assertEqual(r.get_full_name(), 'Aashirya Kaushik')
 
     def test_education(self):
+        '''
+        Test case for get_education
+        '''
         r = Resume.objects.first()
         actual = list(r.get_education())
         expected = list(r.education_set.all())
         self.assertEqual(actual,expected)
 
     def test_experience(self):
+        '''
+        Test case for get_experience
+        '''
         r = Resume.objects.first()
         actual = list(r.get_experience())
         expected = list(r.experience_set.all())
